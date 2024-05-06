a73df11e6d9510bfe4adb99933671c7884c8ac3d
Teemsoha <163581338+Teemsoha@users.noreply.github.com>
2024-05-06 07:40:57 +0300

    Update __init__.py

--- HVAnimeBot/__init__.py
+++ HVAnimeBot/__init__.py
@@ -42,5 +42,5 @@ LOAD = Config.LOAD
 TOKEN = Config.TOKEN
 
 
-updater = tg.Updater(TOKEN, workers=8, use_context=True)
+updater = tg.Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
